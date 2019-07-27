3425d7a8febab6a818996351ea2668937c822739
Martmists <martmists@gmail.com>
2019-07-27 13:17:22 +0200

    [feat/items] Add code for all elements
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/main/java/net/kineticdevelopment/arcana/common/items/elements/venom.java
+++ src/main/java/net/kineticdevelopment/arcana/common/items/elements/venom.java
@@ -4,6 +4,7 @@ import net.kineticdevelopment.arcana.common.creativetab.ModTabGroups;
 import net.minecraft.item.Item;
 
 public class venom extends Item {
+
     public venom() {
         super(new Properties().group(ModTabGroups.MOD_ITEM_GROUP)
         .maxStackSize(64));
