28160168e48ea1a80792802728b99c62fecd8fe9
Martmists <martmists@gmail.com>
2019-07-27 13:20:13 +0200

    [feat/items] Add code for all elements
    Signed-off-by: Martmists <martmists@gmail.com>

---
+++ src/main/java/net/kineticdevelopment/arcana/common/items/elements/metal.java
@@ -0,0 +1,14 @@
+package net.kineticdevelopment.arcana.common.items.elements;
+
+import net.kineticdevelopment.arcana.common.creativetab.ModTabGroups;
+import net.minecraft.item.Item;
+
+public class metal extends Item {
+
+    public metal() {
+        super(new Properties().group(ModTabGroups.MOD_ITEM_GROUP)
+        .maxStackSize(64));
+
+        setRegistryName("metal");
+    }
+}
