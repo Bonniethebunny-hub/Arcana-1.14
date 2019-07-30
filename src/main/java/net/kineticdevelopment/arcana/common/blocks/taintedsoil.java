59d87b238ef853e40287a15643f03823a5c8adb6
AtlasThe1st <45739952+AtlasThe1st@users.noreply.github.com>
2019-07-29 21:21:31 -0500

    Tainted Soil now spreads, TaintLevel needs work , resets every time you restart your game

--- src/main/java/net/kineticdevelopment/arcana/common/blocks/taintedsoil.java
+++ src/main/java/net/kineticdevelopment/arcana/common/blocks/taintedsoil.java
@@ -1,18 +1,209 @@
 package net.kineticdevelopment.arcana.common.blocks;
 
+import java.util.Random;
+
+import net.kineticdevelopment.arcana.common.init.ModBlocks;
+import net.kineticdevelopment.arcana.utilities.TaintLevelHandler;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 public class taintedsoil extends Block {
     public taintedsoil() {
-        super(Block.Properties.create(Material.IRON)
-                .sound(SoundType.STONE)
+        super(Block.Properties.create(Material.SAND)
+                .sound(SoundType.GROUND)
                 .hardnessAndResistance(3.0f)
                 .harvestLevel(0)
                 .harvestTool(ToolType.SHOVEL)
+                .tickRandomly()
         );
         setRegistryName("taintedsoil");
+    }
+    
+    @Override
+    public boolean ticksRandomly(BlockState state) {
+        return true;
+    }
+    
+    public void tick(BlockState state, World worldIn, BlockPos pos, Random random) {
+    	
+    	//Grass
+    	if(worldIn.getBlockState(pos.add(1, 0, 0)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(1, 0, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, 0, 1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(1, 0, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 0, 0)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(-1, 0, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 0, -1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(-1, 0, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 0, 1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(-1, 0, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, 0, -1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(1, 0, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, 0, -1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(0, 0, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, 0, 1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(0, 0, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	
+    	if(worldIn.getBlockState(pos.add(1, 1, 0)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(1, 1, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, 1, 1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(1, 1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 1, 0)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(-1, 1, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 1, -1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(-1, 1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 1, 1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(-1, 1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, 1, -1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(1, 1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, 1, -1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(0, 1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, 1, 1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(0, 1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	
+    	if(worldIn.getBlockState(pos.add(1, -1, 0)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(1, -1, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, -1, 1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(1, -1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, -1, 0)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(-1, -1, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, -1, -1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(-1, -1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, -1, 1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(-1, -1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, -1, -1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(1, -1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, -1, -1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(0, -1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, -1, 1)).getBlock().equals(Blocks.GRASS_BLOCK)) {
+    		worldIn.setBlockState(pos.add(0, -1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	
+    	//Dirt
+    	if(worldIn.getBlockState(pos.add(1, 0, 0)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(1, 0, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, 0, 1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(1, 0, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 0, 0)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(-1, 0, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 0, -1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(-1, 0, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 0, 1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(-1, 0, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, 0, -1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(1, 0, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, 0, -1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(0, 0, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, 0, 1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(0, 0, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	
+    	if(worldIn.getBlockState(pos.add(1, 1, 0)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(1, 1, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, 1, 1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(1, 1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 1, 0)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(-1, 1, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 1, -1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(-1, 1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, 1, 1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(-1, 1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, 1, -1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(1, 1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, 1, -1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(0, 1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, 1, 1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(0, 1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	
+    	if(worldIn.getBlockState(pos.add(1, -1, 0)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(1, -1, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, -1, 1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(1, -1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, -1, 0)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(-1, -1, 0), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, -1, -1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(-1, -1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(-1, -1, 1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(-1, -1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(1, -1, -1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(1, -1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, -1, -1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(0, -1, -1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    	if(worldIn.getBlockState(pos.add(0, -1, 1)).getBlock().equals(Blocks.DIRT)) {
+    		worldIn.setBlockState(pos.add(0, -1, 1), ModBlocks.TAINTEDSOIL.getDefaultState());
+    	}
+    }
+    
+    @Override
+    public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+    	if(!worldIn.isRemote) {
+    		if(player.getHeldItemMainhand().getItem() instanceof AxeItem)
+        	{
+        		TaintLevelHandler.increaseTaintLevel((World) worldIn, 10);
+        		return true;
+        	}
+        	else
+        	{
+        		return false;
+        	}
+    	}
+    	else {
+    		return false;
+    	}
     }
 }
--- src/main/java/net/kineticdevelopment/arcana/utilities/TaintLevelHandler.java
+++ src/main/java/net/kineticdevelopment/arcana/utilities/TaintLevelHandler.java
@@ -13,88 +13,97 @@ import net.minecraft.world.World;
 
 public class TaintLevelHandler {
 	public static void createTaintLevelFile(World world) {
-		final File file = new File("saves/"+world.getWorldInfo().getWorldName(), "taintLevel.txt");
-		try(FileWriter fw = new FileWriter("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt", false); BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
-			if(!file.exists()) {
-				file.createNewFile();
-				out.print("0");
-				Constants.LOGGER.info("Created Taint Level File");
-			}
-		} 
-		catch (IOException e) {
-			
-	    }
+		if(!world.isRemote) {
+			final File file = new File("saves/"+world.getWorldInfo().getWorldName(), "taintLevel.txt");
+			try(FileWriter fw = new FileWriter("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt", false); BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
+				if(!file.exists()) {
+					file.createNewFile();
+					out.print("0");
+					Constants.LOGGER.info("Created Taint Level File");
+				}
+			} 
+			catch (IOException e) {
+				
+		    }
+		}
 	}
     
-    public int getTaintLevel(World world) {
-    	int taintLevel;
-        try (BufferedReader br = Files.newBufferedReader(Paths.get("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt"))) {
-        	if(br.readLine() != null) {
-        		taintLevel = Integer.parseInt(br.readLine());
-                return taintLevel;
-        	}
-        	if(br.readLine() == null) {
-        		taintLevel = 0;
-                return taintLevel;
-        	}
-        } 
-        catch(IOException e) {
-            System.err.format("IOException: %s%n", e);
-        }
-        return 2147483647;
+    public static int getTaintLevel(World world) {
+    	if(!world.isRemote) {
+    		int taintLevel;
+            try (BufferedReader br = Files.newBufferedReader(Paths.get("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt"))) {
+            	if(br.readLine() != null) {
+            		taintLevel = Integer.parseInt(br.readLine());
+                    return taintLevel;
+            	}
+            	if(br.readLine() == null) {
+            		taintLevel = 0;
+                    return taintLevel;
+            	}
+            } 
+            catch(IOException e) {
+                System.err.format("IOException: %s%n", e);
+            }
+            return 2147483647;
+    	}
+    	return 0;
     }
     
     public static void increaseTaintLevel(World world, int amount) {
-    	int taintLevel = 2147483647;
-        try (BufferedReader br = Files.newBufferedReader(Paths.get("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt"))) {
-        	String s = br.readLine();
-            if(s != null) {
-            	taintLevel = Integer.parseInt(s);
-                taintLevel = taintLevel + amount;
+    	if(!world.isRemote) {
+    		int taintLevel = 2147483647;
+            try (BufferedReader br = Files.newBufferedReader(Paths.get("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt"))) {
+            	String s = br.readLine();
+                if(s != null) {
+                	taintLevel = Integer.parseInt(s);
+                    taintLevel = taintLevel + amount;
+                }
+                
+                if(s == null) {
+                	taintLevel = 0;
+                    taintLevel = taintLevel + amount;
+                }
+                System.out.println("Taint Level is now "+taintLevel);
+            } 
+            catch(IOException e) {
+                System.err.format("IOException: %s%n", e);
             }
             
-            if(s == null) {
-            	taintLevel = 0;
-                taintLevel = taintLevel + amount;
-            }
-            System.out.println("Taint Level is now "+taintLevel);
-        } 
-        catch(IOException e) {
-            System.err.format("IOException: %s%n", e);
-        }
-        
-		try(FileWriter fw = new FileWriter("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt", false); BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
-			out.print(taintLevel);
-		} 
-		catch (IOException e) {
-			
-	    }
+    		try(FileWriter fw = new FileWriter("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt", false); BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
+    			out.print(taintLevel);
+    		} 
+    		catch (IOException e) {
+    			
+    	    }
+    	}
     }
     
     public static void decreaseTaintLevel(World world, int amount) {
-    	int taintLevel = 2147483647;
-        try (BufferedReader br = Files.newBufferedReader(Paths.get("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt"))) {
-        	String s = br.readLine();
-            if(s != null) {
-            	taintLevel = Integer.parseInt(s);
-                taintLevel = taintLevel - amount;
+    	if(!world.isRemote) {
+    		int taintLevel = 2147483647;
+            try (BufferedReader br = Files.newBufferedReader(Paths.get("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt"))) {
+            	String s = br.readLine();
+                if(s != null) {
+                	taintLevel = Integer.parseInt(s);
+                    taintLevel = taintLevel - amount;
+                }
+                
+                if(s == null) {
+                	taintLevel = 0;
+                    taintLevel = taintLevel - amount;
+                }
+                System.out.println("Taint Level is now "+taintLevel);
+            } 
+            catch(IOException e) {
+                System.err.format("IOException: %s%n", e);
             }
             
-            if(s == null) {
-            	taintLevel = 0;
-                taintLevel = taintLevel - amount;
-            }
-            System.out.println("Taint Level is now "+taintLevel);
-        } 
-        catch(IOException e) {
-            System.err.format("IOException: %s%n", e);
-        }
-        
-		try(FileWriter fw = new FileWriter("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt", false); BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
-			out.print(taintLevel);
-		} 
-		catch (IOException e) {
-			
-	    }
+    		try(FileWriter fw = new FileWriter("saves/"+world.getWorldInfo().getWorldName()+"/taintLevel.txt", false); BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
+    			out.print(taintLevel);
+    		} 
+    		catch (IOException e) {
+    			
+    	    }
+    	}
     }
 }
