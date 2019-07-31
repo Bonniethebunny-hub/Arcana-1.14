8bb177af0c21b9f84f713b88c861e792f3197f4a
MaxIsH0t <43427964+MaxIsH0t@users.noreply.github.com>
2019-07-31 17:37:04 -0600

    Still working on the GUIEvents

--- src/main/java/net/kineticdevelopment/arcana/client/GuiEvents.java
+++ src/main/java/net/kineticdevelopment/arcana/client/GuiEvents.java
@@ -31,38 +31,37 @@ public class GuiEvents {
             if (player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem() instanceof AspectGogglesHelmet) {
                 GlStateManager.pushMatrix();
 
-                // X POS
-                BlockPos blockPosX = new BlockPos(player.posX - 1, player.posY, player.posZ);
-                BlockPos blockPo2X = new BlockPos(player.posX - 2, player.posY, player.posZ);
-                BlockPos blockPo3X = new BlockPos(player.posX - 3, player.posY, player.posZ);
+                BlockPos blockPos = new BlockPos(player.posX, player.posY, player.posZ);
+                int x;
+                int y;
+                int z;
 
-                // Y POS
-                BlockPos blockPosY = new BlockPos(player.posX, player.posY - 1, player.posZ);
-                BlockPos blockPo2Y = new BlockPos(player.posX, player.posY - 2, player.posZ);
-                BlockPos blockPo3Y = new BlockPos(player.posX, player.posY - 3, player.posZ);
+                for (x = blockPos.getX() - 2; x < 3; x++) {
+                    for (z = blockPos.getZ() - 2; z < 3; z++) {
+                        for (y = blockPos.getY() - 2; y < 3; y++) {
+                            System.out.println(x + "," + y + "," + z);
 
-                // Z POS
-                BlockPos blockPosZ = new BlockPos(player.posX, player.posY, player.posZ - 1);
-                BlockPos blockPo2Z = new BlockPos(player.posX, player.posY, player.posZ - 2);
-                BlockPos blockPo3Z = new BlockPos(player.posX, player.posY, player.posZ - 3);
+                            /**
+                            // ASPECT GOGGLES
+                            RenderHelper.renderTextScaled(TextFormatting.RED + "(Press the F key to scan blocks around you)", 10, 10, 0xFFFFFF, 0.8D);
+                            RenderHelper.renderTextScaled(TextFormatting.WHITE + "Aspect Goggles " + TextFormatting.GREEN + "X: " + TextFormatting.WHITE + ""
+                                            + world.getBlockState(blockPosX).getPickBlock(null, world, blockPosX, playerEntity).toString() + ", "
+                                            + world.getBlockState(blockPo2X).getPickBlock(null, world, blockPo2X, playerEntity).toString() + ", "
+                                            + world.getBlockState(blockPo3X).getPickBlock(null, world, blockPo3X, playerEntity).toString() + " " +
 
-                // ASPECT GOGGLES
-                RenderHelper.renderTextScaled(TextFormatting.RED + "(Press the F key to scan blocks around you)", 10, 10, 0xFFFFFF, 0.8D);
-                RenderHelper.renderTextScaled(TextFormatting.WHITE + "Aspect Goggles " + TextFormatting.GREEN + "X: " + TextFormatting.WHITE + ""
-                                + world.getBlockState(blockPosX).getPickBlock(null, world, blockPosX, playerEntity).toString() + ", "
-                                + world.getBlockState(blockPo2X).getPickBlock(null, world, blockPo2X, playerEntity).toString() + ", "
-                                + world.getBlockState(blockPo3X).getPickBlock(null, world, blockPo3X, playerEntity).toString() + " " +
+                                            TextFormatting.GREEN + "Y: " + TextFormatting.WHITE + ""
+                                            + world.getBlockState(blockPosY).getPickBlock(null, world, blockPosY, playerEntity).toString() + ", "
+                                            + world.getBlockState(blockPo2Y).getPickBlock(null, world, blockPo2Y, playerEntity).toString() + ", "
+                                            + world.getBlockState(blockPo3Y).getPickBlock(null, world, blockPo3Y, playerEntity).toString() + " " +
 
-                                TextFormatting.GREEN + "Y: " + TextFormatting.WHITE + ""
-                        + world.getBlockState(blockPosY).getPickBlock(null, world, blockPosY, playerEntity).toString() + ", "
-                        + world.getBlockState(blockPo2Y).getPickBlock(null, world, blockPo2Y, playerEntity).toString() + ", "
-                        + world.getBlockState(blockPo3Y).getPickBlock(null, world, blockPo3Y, playerEntity).toString() + " " +
-
-                                TextFormatting.GREEN + "Z: " + TextFormatting.WHITE + ""
-                                + world.getBlockState(blockPosZ).getPickBlock(null, world, blockPosZ, playerEntity).toString() + ", "
-                                + world.getBlockState(blockPo2Z).getPickBlock(null, world, blockPo2Z, playerEntity).toString() + ", "
-                                + world.getBlockState(blockPo3Z).getPickBlock(null, world, blockPo3Z, playerEntity).toString(),
-                        10, 20, 0xFFFFFF, 0.6D);
+                                            TextFormatting.GREEN + "Z: " + TextFormatting.WHITE + ""
+                                            + world.getBlockState(blockPosZ).getPickBlock(null, world, blockPosZ, playerEntity).toString() + ", "
+                                            + world.getBlockState(blockPo2Z).getPickBlock(null, world, blockPo2Z, playerEntity).toString() + ", "
+                                            + world.getBlockState(blockPo3Z).getPickBlock(null, world, blockPo3Z, playerEntity).toString(),
+                                    10, 20, 0xFFFFFF, 0.6D);**/
+                        }
+                    }
+                }
                 
                 GlStateManager.disableBlend();
                 GlStateManager.popMatrix();
