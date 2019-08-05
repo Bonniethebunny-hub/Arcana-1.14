773de3ee96c44938fbbc21090c51617815670207
SleepingTea98 <chrisweston98@gmail.com>
2019-08-05 20:41:31 +0100

    Finnished up tainted biome
    -

--- src/main/java/net/kineticdevelopment/arcana/common/biomes/TaintBiome.java
+++ src/main/java/net/kineticdevelopment/arcana/common/biomes/TaintBiome.java
@@ -12,18 +12,20 @@ import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
 
 public class TaintBiome extends Biome {
 
-        protected static final SurfaceBuilderConfig TaintBiome = new SurfaceBuilderConfig(ModBlocks.TAINTEDSOIL.getDefaultState(), Blocks.PODZOL.getDefaultState(), Blocks.CLAY.getDefaultState());
+        protected static final SurfaceBuilderConfig TaintBiome = new SurfaceBuilderConfig(ModBlocks.TAINTEDSOIL.getDefaultState(), Blocks.DIRT.getDefaultState(), Blocks.CLAY.getDefaultState());
 
 	public TaintBiome()
         {
-            super((new Biome.Builder()).surfaceBuilder(new ConfiguredSurfaceBuilder<>(SurfaceBuilder.DEFAULT, TaintBiome)).precipitation(Biome.RainType.NONE).category(Biome.Category.DESERT).depth(0.125F).scale(0.05F).temperature(1000.8F).downfall(0.4F).waterColor(4159204).waterFogColor(329011).parent((String)null));
-            this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(ModEntities.ARCANA_TAINTED_CHICKEN, 100, 4, 4));
+            super((new Biome.Builder()).surfaceBuilder(new ConfiguredSurfaceBuilder<>(SurfaceBuilder.DEFAULT, TaintBiome)).precipitation(RainType.RAIN).category(Category.FOREST).depth(0.125F).scale(0.05F).temperature(9999.0F).downfall(0.4F).waterColor(6684774).waterFogColor(6684774).parent((String)null));
+            //this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(ModEntities.ARCANA_TAINTED_CHICKEN, 0, 0, 0));
         }
 
-        @Override
+        /*@Override
         public int getSkyColorByTemp(float currentTemperature)
         {
             return 0xE0B041;
         }
+
+         */
 
 }
