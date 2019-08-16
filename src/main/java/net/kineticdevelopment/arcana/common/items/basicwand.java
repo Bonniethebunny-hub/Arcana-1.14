dae6cd108d48ff0698c495459810dabdadfce022
AtlasThe1st <45739952+AtlasThe1st@users.noreply.github.com>
2019-08-15 23:30:04 -0500

    Blocks can now be assigned aspects

--- src/main/java/net/kineticdevelopment/arcana/common/items/basicwand.java
+++ src/main/java/net/kineticdevelopment/arcana/common/items/basicwand.java
@@ -1,7 +1,5 @@
 package net.kineticdevelopment.arcana.common.items;
 
-import java.util.ArrayList;
-
 import net.kineticdevelopment.arcana.common.creativetab.ModTabGroups;
 import net.kineticdevelopment.arcana.utilities.AspectCollectionHandler;
 import net.minecraft.item.Item;
@@ -18,9 +16,9 @@ public class basicwand extends Item {
     
     public ActionResultType onItemUse(ItemUseContext context) {
     	if(!context.getWorld().isRemote) {
-    		ArrayList<Item> aspects = AspectCollectionHandler.getBlockAspects(context.getWorld().getBlockState(context.getPos()).getBlock());
-    		for(int i=0; i < aspects.size(); i++) {
-    			System.out.println(aspects.get(i));
+    		Item[] aspects = AspectCollectionHandler.getBlockAspects(context.getWorld().getBlockState(context.getPos()).getBlock());
+    		for(int i=0; i < aspects.length; i++) {
+    			System.out.println(aspects[i]);
     		}
     	}
         return ActionResultType.PASS;
--- src/main/java/net/kineticdevelopment/arcana/utilities/AspectCollectionHandler.java
+++ src/main/java/net/kineticdevelopment/arcana/utilities/AspectCollectionHandler.java
@@ -1,8 +1,7 @@
 package net.kineticdevelopment.arcana.utilities;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Map.Entry;
 
 import net.kineticdevelopment.arcana.common.init.ItemInit;
 import net.minecraft.block.Block;
@@ -11,19 +10,23 @@ import net.minecraft.item.Item;
 
 public class AspectCollectionHandler {
 	@SuppressWarnings("serial")
-	static HashMap<Block, Item> BlockAspects = new HashMap<Block, Item>() {{
-		put(Blocks.DIRT, ItemInit.EARTH);
+	static HashMap<Block, Item[]> BlockAspects = new HashMap<Block, Item[]>() {{
+		put(Blocks.DIRT, new Item[] {ItemInit.EARTH});
+		put(Blocks.ICE, new Item[] {ItemInit.ICE, ItemInit.WATER});
 	}};
 	
-	public static ArrayList<Item> getBlockAspects(Block block) {
-		ArrayList<Item> aspects = new ArrayList<Item>();
-		for (Map.Entry<Block, Item> entry : BlockAspects.entrySet()) {
-			for(int i=0; i < BlockAspects.size(); i++) {
-				if (block.equals(entry.getKey().getBlock())) {
-	            	aspects.add(entry.getValue().getItem());
-	            }
-			}
+	/**
+	 * Returns an Item Array with all the Aspects of a block
+	 * @param block
+	 * @return Item[]
+	 */
+	public static Item[] getBlockAspects(Block block) {
+		for (Entry<Block, Item[]> entry : BlockAspects.entrySet()) {
+			if (block.equals(entry.getKey().getBlock())) {
+            	Item[] aspects = entry.getValue(); 
+            	return aspects;
+            }
         }
-		return aspects;
+		return null;
 	}
 }
