6752bc64513ce6fb430bcef58a522bfea5fb108f
SleepingTea98 <chrisweston98@gmail.com>
2019-09-05 18:54:53 +0100

    Update planks
    -

---
+++ src/main/java/net/kineticdevelopment/arcana/common/blocks/tainted/tainted_planks_stairs.java
@@ -0,0 +1,18 @@
+package net.kineticdevelopment.arcana.common.blocks.tainted;
+
+import net.kineticdevelopment.arcana.common.init.BlockInit;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+
+public class tainted_planks_stairs extends StairsBlock {
+
+    public tainted_planks_stairs() {
+
+        super(BlockInit.TAINTEDOAKPLANKS.getDefaultState());
+
+    }
+}
