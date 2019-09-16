a30781f6cd08a122f9068848578ae25318c52727
SleepingTea98 <chrisweston98@gmail.com>
2019-09-16 05:35:25 +0100

    Added Items
    -

--- src/main/java/kineticdevelopment/api/items/ArcanaItems.java
+++ src/main/java/kineticdevelopment/api/items/ArcanaItems.java
@@ -1,7 +1,9 @@
 package kineticdevelopment.api.items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 
 public class ArcanaItems {
 
     public static Item Thaumonomicon;
+
 }
---
+++ src/main/java/kineticdevelopment/init/ModItems.java
@@ -0,0 +1,30 @@
+package kineticdevelopment.init;
+
+
+import kineticdevelopment.api.items.ArcanaItems;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import static kineticdevelopment.api.items.ArcanaItems.*;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModItems {
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> even)
+    {
+        Thaumonomicon = registerItem(new Item(new Item.Properties().maxStackSize(1)), "thaumonomicon");
+    }
+
+    public static Item registerItem(Item item, String name) {
+
+        item.setRegistryName(name);
+        ForgeRegistries.ITEMS.register(item);
+        return item;
+    }
+
+
+}
