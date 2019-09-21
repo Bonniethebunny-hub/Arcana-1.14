6b5df3384904817bbe0c490287e6850a07b1f6ed
SleepingTea98 <chrisweston98@gmail.com>
2019-09-21 22:42:08 +0100

    Started work on entities
    -

---
+++ src/main/java/kineticdevelopment/init/ModEntities.java
@@ -0,0 +1,46 @@
+/**
+ * All items should be listed here.
+ */
+
+package kineticdevelopment.init;
+
+
+import kineticdevelopment.common.utils.creativetab.ArcanaItemGroup;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import static kineticdevelopment.api.items.ArcanaItems.*;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEntities {
+
+    @SubscribeEvent
+    public static void registerEntities(RegistryEvent.Register<EntityType<?>> event)
+    {
+
+
+    }
+
+    public static <T extends Entity> EntityType<T> createEntity(EntityType.IFactory<T> factory, EntityClassification classification, String name, int trackingRange, int updateFrequency, boolean sendsVelocityUpdates)
+    {
+        ResourceLocation location = new ResourceLocation("arana", name);
+        EntityType<T> type = EntityType.Builder.<T>create(factory, classification).setTrackingRange(trackingRange).setUpdateInterval(updateFrequency).setShouldReceiveVelocityUpdates(sendsVelocityUpdates).build(location.toString());
+        type.setRegistryName(name);
+        ForgeRegistries.ENTITIES.register(type);
+        return type;
+    }
+
+
+}
+
+
