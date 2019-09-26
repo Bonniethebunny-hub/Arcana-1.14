55bcb3b60fc48aa32bd51eed5c1b5bf01544bc59
MaxIsH0t <43427964+MaxIsH0t@users.noreply.github.com>
2019-09-25 20:07:55 -0600

    JackOLatern class code cleanup

--- src/main/java/kineticdevelopment/common/blocks/tainted/TraintedJackOLatern.java
+++ src/main/java/kineticdevelopment/common/blocks/tainted/TraintedJackOLatern.java
@@ -2,9 +2,8 @@ package kineticdevelopment.common.blocks.tainted;
 
 import net.minecraft.block.CarvedPumpkinBlock;
 
-public class TraintedJackOLatern extends CarvedPumpkinBlock
-{
-    public TraintedJackOLatern(Properties p_i48432_1_) {
-        super(p_i48432_1_);
+public class TraintedJackOLatern extends CarvedPumpkinBlock {
+    public TraintedJackOLatern(Properties properties) {
+        super(properties);
     }
 }
