4dfe3eebbca2a9834dfc9351e8074271067235f9
AtlasThe1st <45739952+AtlasThe1st@users.noreply.github.com>
2019-09-27 03:14:29 -0500

    Many changes, additions, and fixes
    Added BlockHasNoAspectsException
    Added addBlockToPlayerResearchBlackList function
    Added isBlockOnPlayerResearchBlackList boolean
    Started work on adding more of an existing aspect to a player's pool
    Moved "You just learned" message to the AspectPoolHandler where it can be processed better
    Made items use addBlockAspectsToPlayer instead of individually adding them in the item's class

--- src/main/java/kineticdevelopment/common/items/Thaumonomicon.java
+++ src/main/java/kineticdevelopment/common/items/Thaumonomicon.java
@@ -1,14 +1,15 @@
 package kineticdevelopment.common.items;
 
 import java.util.List;
+
 import javax.annotation.Nullable;
+
 import kineticdevelopment.api.aspects.Aspect.AspectType;
 import kineticdevelopment.api.aspects.AspectNotFoundException;
-import kineticdevelopment.client.gui.GuiThaumonomicon;
+import kineticdevelopment.api.aspects.BlockHasNoAspectsException;
 import kineticdevelopment.common.utils.aspectpool.AspectPoolHandler;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
@@ -17,12 +18,10 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fml.network.NetworkHooks;
 
 public class Thaumonomicon extends Item {
 
@@ -35,12 +34,7 @@ public class Thaumonomicon extends Item {
     	if(!context.getWorld().isRemote) {
     		
     		if(!context.isPlacerSneaking()) {
-        		AspectType[] aspects = AspectPoolHandler.getBlockAspects(context.getWorld().getBlockState(context.getPos()).getBlock());
-
-            	for(int i=0; i<aspects.length; i++) {
-            		AspectPoolHandler.addAspectToPlayer(context.getPlayer(), context.getWorld(), aspects[i], 10);
-            		context.getPlayer().sendMessage(new StringTextComponent(TextFormatting.GREEN + "You have just learned the " + TextFormatting.RED +aspects[i] + TextFormatting.GREEN + " aspect type!"));
-            	}
+        		AspectPoolHandler.addBlockAspectsToPlayer(context.getWorld().getBlockState(context.getPos()).getBlock(), context.getPlayer(), context.getWorld());
         	}
 
         	else {
@@ -67,6 +61,6 @@ public class Thaumonomicon extends Item {
 	
 	@OnlyIn(Dist.CLIENT)
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-		tooltip.add(new StringTextComponent(TextFormatting.RED + "Currently being used for testing with aspect collection"));
+		tooltip.add(new StringTextComponent("Currently being used for testing with aspect collection"));
 	}
 }
--- src/main/java/kineticdevelopment/common/utils/aspectpool/AspectPoolHandler.java
+++ src/main/java/kineticdevelopment/common/utils/aspectpool/AspectPoolHandler.java
@@ -19,15 +19,20 @@ import javax.annotation.Nullable;
 import kineticdevelopment.api.aspects.Aspect;
 import kineticdevelopment.api.aspects.Aspect.AspectType;
 import kineticdevelopment.api.aspects.AspectNotFoundException;
+import kineticdevelopment.api.aspects.BlockHasNoAspectsException;
 import kineticdevelopment.common.utils.Constants;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 /**
  * Handles anything to do with the aspect pool
  * @author Atlas
+ * @see Aspect
  */
 public class AspectPoolHandler {
 	
@@ -43,9 +48,9 @@ public class AspectPoolHandler {
 	
 	 * Returns an Item Array with all the Aspects of a block
 	 * @param block
-	 * @return Item[]
+	 * @return AspectType[]
 	 */
-	public static AspectType[] getBlockAspects(Block block) {
+	public static AspectType[] getBlockAspects(Block block) throws BlockHasNoAspectsException {
 		
 		for (Entry<Block, AspectType[]> entry : BlockAspects.entrySet()) {
 			if (block.equals(entry.getKey().getBlock())) {
@@ -53,8 +58,23 @@ public class AspectPoolHandler {
 				return aspects;
 			}
 		}
-		return null;
+		throw new BlockHasNoAspectsException("Block "+block.getNameTextComponent().getFormattedText()+" has no assigned aspects!");
 	 }
+	
+	public static void addBlockAspectsToPlayer(Block block, PlayerEntity player, World world) {
+		AspectType[] aspects;
+		try {
+			aspects = AspectPoolHandler.getBlockAspects(block);
+			
+        	for(int i=0; i<aspects.length; i++) {
+        		AspectPoolHandler.addAspectToPlayer(player, world, aspects[i], 10);
+        	}
+		} catch (BlockHasNoAspectsException e) {
+			e.printStackTrace();
+		}
+		
+		AspectPoolHandler.addBlockToPlayerResearchBlackList(block, player, world);
+	}
 
 	/**
 	 * Adds the specified aspect to the player's pool
@@ -81,7 +101,46 @@ public class AspectPoolHandler {
 				
 				if(!aspectlist.contains(aspect)) {
 					out.println(aspect.name()+", "+amount);
+					
+					player.sendMessage(new StringTextComponent(TextFormatting.GREEN + "You have just learned the " + TextFormatting.RED + aspect.name() + TextFormatting.GREEN + " aspect type!"));
 				}
+				
+				/**
+				 * Doesn't work yet, straight-up wipes the whole file instead of the one line I want it to
+				 */
+//				if(aspectlist.contains(aspect)) {
+//					try (BufferedReader br = Files.newBufferedReader(playerAspectData.toPath()))
+//		    		{
+//		    			FileReader fr = new FileReader(playerAspectData);
+//		    		    LineNumberReader lnr = new LineNumberReader(fr);
+//		    		    String line;
+//		    		    
+//		    		    line=br.readLine();
+//		    		    
+//			        	try(FileWriter fw2 = new FileWriter(playerAspectData, true);
+//			    			BufferedWriter bw2 = new BufferedWriter(fw2);
+//			    			PrintWriter out2 = new PrintWriter(bw2))
+//			    		{
+//			        		while ((line = br.readLine()) != null) {
+//								for(int i=0; i<lnr.getLineNumber(); i++) {
+//									if(Aspect.getAspectByName(line.substring(0, line.indexOf(", "))) == aspect) {
+//										out.write(aspect.name()+", "+String.valueOf((getPlayerAspectAmount(player, aspect, world) + amount)));
+//									}
+//									
+//									//Debug
+//									System.out.println(aspect.name());
+//									
+//								}
+//							}
+//			    		}
+//		    	 
+//		    	        lnr.close();
+//		    		}
+//		    		catch(IOException e)
+//		    		{
+//		    			e.printStackTrace();
+//		    		}
+//				}
 			} 
 			
 			catch (IOException e) {
@@ -224,5 +283,98 @@ public class AspectPoolHandler {
 		else {
 			throw new AspectNotFoundException("Aspect "+type.name()+" not found for player "+player.getName().getFormattedText());
 		}
+	}
+	
+	public static void addBlockToPlayerResearchBlackList(Block block, PlayerEntity player, World world) {
+		try {
+	    	  File researchDataDir = new File(world.getWorldInfo().getWorldName(), "researchData");
+
+	    	  researchDataDir.mkdirs();
+
+	    	  
+
+	    	  File playerResearchBlackList = new File(researchDataDir, player.getCachedUniqueIdString()+".researchBlackList");
+
+	    	  if(!playerResearchBlackList.exists()) {
+
+	    		  playerResearchBlackList.createNewFile();
+
+	    	  }
+
+	    	  try(FileWriter fw = new FileWriter(playerResearchBlackList, true);
+	    	      BufferedWriter bw = new BufferedWriter(fw);
+	    		  PrintWriter out = new PrintWriter(bw)) {
+	    		  
+	    		  if(!(isBlockOnPlayerResearchBlackList(block, player, world))) {
+	    			  out.println(Item.getIdFromItem(block.asItem()));
+	    		  }
+	  		  } 
+
+	  		  	catch (IOException e) {
+
+	  		  		Constants.LOGGER.warn("Failed to write to "+player.getCachedUniqueIdString()+".researchBlackList");
+
+	  			}
+
+	      } catch (Exception var5) {
+	    	  var5.printStackTrace();
+
+	      }
+	}
+	
+	public static boolean isBlockOnPlayerResearchBlackList(Block block, PlayerEntity player, World world) {
+		int lineCount = 0;
+		
+		try {
+			File researchDataDir = new File(world.getWorldInfo().getWorldName(), "researchData");
+			
+			researchDataDir.mkdirs();
+			
+			File playerResearchBlackList = new File(researchDataDir, player.getCachedUniqueIdString()+".researchBlackList");
+
+			if(!playerResearchBlackList.exists()) {
+				return false;
+			}
+			
+			FileReader fr = new FileReader(playerResearchBlackList);
+			
+			LineNumberReader lnr = new LineNumberReader(fr);
+			
+			while (lnr.readLine() != null) {
+				lineCount++;
+			}
+			
+			//Debug
+			System.out.println("Total number of lines : " + lineCount);
+			
+			lnr.close();
+		}
+
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			
+			try(BufferedReader br = Files.newBufferedReader(Paths.get(world.getWorldInfo().getWorldName(), "researchData", player.getCachedUniqueIdString()+".researchBlackList"))) {
+				
+				String line;
+				
+				while ((line = br.readLine()) != null) {
+					for(int i=0; i<lineCount; i++) {
+						if(Item.getItemById(Integer.parseInt(line)) == block.asItem()) {
+							return true;
+						}
+						
+					}
+				}
+			} 
+
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return false;
 	}
 }
