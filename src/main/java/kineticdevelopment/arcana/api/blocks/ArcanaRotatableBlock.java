0f7980fe9db90e78855d63c2931880fe970aacf8
SleepingTea98 <chrisweston98@gmail.com>
2019-10-10 21:29:57 +0100

    Stuff now rotates :D
    -

--- src/main/java/kineticdevelopment/arcana/api/blocks/ArcanaRotatableBlock.java
+++ src/main/java/kineticdevelopment/arcana/api/blocks/ArcanaRotatableBlock.java
@@ -1,6 +1,13 @@
 package kineticdevelopment.arcana.api.blocks;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.CarvedPumpkinBlock;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
 
 /**
  * All Arcana rotatable blocks should extend this class
@@ -9,5 +16,19 @@ public class ArcanaRotatableBlock extends CarvedPumpkinBlock {
     public ArcanaRotatableBlock(Properties properties) {
         super(properties);
 
+    }
+
+    @Override
+    public boolean isSolid(BlockState state) {
+        return false;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return VoxelShapes.fullCube();
+    }
+
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.TRANSLUCENT;
     }
 }
