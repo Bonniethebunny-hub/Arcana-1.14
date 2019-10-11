97c85a58f93d6e6b200678a3fe306ee4b47b86d1
SleepingTea98 <chrisweston98@gmail.com>
2019-10-11 22:29:53 +0100

    Removed Unused Code
    -

--- src/main/java/kineticdevelopment/arcana/common/utils/blocks/IBlockPosQuery.java
+++
@@ -1,8 +0,0 @@
-package kineticdevelopment.arcana.common.utils.blocks;
-
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorld;
-
-public interface IBlockPosQuery {
-     boolean matches(IWorld world, BlockPos pos);
-}
